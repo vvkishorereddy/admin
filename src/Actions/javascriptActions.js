aada2ff3074f040b81efc4a52620776d5c43089f
kishore Reddy <vvkishorereddy@gmail.com>
2019-04-01 23:28:30 +0530

    changes

--- src/Actions/javascriptActions.js
+++ src/Actions/javascriptActions.js
@@ -1,27 +1,8 @@
 import { JAVASCRIPT_LIST_ACTION } from "../constants";
 
-const data = [
-  {
-    id: 1,
-    question: "What is JavaScript?",
-    answer: `JavaScript is a client-side as well as server side scripting language
-    that can be inserted into HTML pages and is understood by web browsers.
-    JavaScript is also an Object based Programming language`,
-    status: true
-  },
-  {
-    id: 2,
-    question: "What is JavaScript List?",
-    answer: `List JavaScript is a client-side as well as server side scripting language ofetr
-    that can be inserted into HTML pages and is understood by web browsers.
-    JavaScript is also an Object based Programming language`,
-    status: false
-  }
-];
-
 const fetchData = async () => {
   const response = await fetch(
-    "https://raw.githubusercontent.com/vvkishorereddy/admin/master/public/data/jqa.json"
+    "https://raw.githubusercontent.com/vvkishorereddy/admin/master/public/data/javascript.json"
   );
   return await response.json();
 };
