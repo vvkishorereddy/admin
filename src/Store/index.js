37f565e8c5eddcee731c373acbcf8e275b6ff0cb
kishore Reddy <vvkishorereddy@gmail.com>
2019-04-01 21:56:10 +0530

    changes

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { logger } from "redux-logger";
+import thunk from "redux-thunk";
 import reducer from "../Reducers";
 
-export default createStore(reducer, {}, applyMiddleware(logger));
+export default createStore(reducer, {}, applyMiddleware(logger, thunk));
